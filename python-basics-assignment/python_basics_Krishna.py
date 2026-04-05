e7844a00fd2b6e439743ad5d47dcbb722482f3d0
Krishna <leelakrishna682@gmail.com>
2026-04-05 15:40:35 +0530

    Add basic variable assignments and arithmetic tasks

--- python-basics-assignment/python_basics_Krishna.py
+++ python-basics-assignment/python_basics_Krishna.py
@@ -1,1 +1,23 @@
+Name = "Krishna"
+Age = 32
+Height = 5.10
+is_Student = False
 
+#Task 1: Print the variables
+print(Name)
+print(Age)
+print(Height)
+print(is_Student)
+
+#Task 2: Print the variables in a single line
+print(f"Name: {Name} | Age: {Age} | Height: {Height} | Student: {is_Student}")
+
+#Task 3 : Arithmetic Operations Using the age variable
+
+Age_in_months = Age * 12
+Age_in_days = Age * 365
+remainder = Age % 7
+print(f"Age in months: {Age_in_months}")
+print(f"Age in days: {Age_in_days}")
+print(f"Remainder when age is divided by 7: {remainder}")
+print(f"Age raised to the power of 2: {Age ** 2}")
